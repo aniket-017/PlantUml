dcdfe20349894e0833866a0dcb9f091adde12c66
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2025-09-22 17:27:19 +0530

    changed python to python3

--- plantuml2/ecosystem.config.js
+++ plantuml2/ecosystem.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-  apps: [
-    {
-      name: "PlantUml",
-      script: "uvicorn",
-      args: "app.main:app --reload --host 154.53.42.27 --port 2004",
-      cwd: "/home/admin/PlantUML/plantuml2",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "production",
+    apps: [
+      {
+        name: "PlantUml",
+        script: "uvicorn",
+        args: "app.main:app --reload --host 154.53.42.27 --port 2004",
+        cwd: "/home/admin/PlantUML/plantuml2",
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "1G",
+        interpreter: "python3",   // 👈 Important fix
+        env: {
+          NODE_ENV: "production",
+        },
+        error_file: "./logs/err.log",
+        out_file: "./logs/out.log",
+        log_file: "./logs/combined.log",
+        time: true,
       },
-      error_file: "./logs/err.log",
-      out_file: "./logs/out.log",
-      log_file: "./logs/combined.log",
-      time: true,
-    },
-  ],
-};
+    ],
+  };
+  
